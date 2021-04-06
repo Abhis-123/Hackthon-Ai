fa3dde5fd8444fedb94dafd91e868fe394ef0f78
abhishek pandir <me19b069@smail.iitm.ac.in>
2021-04-06 10:56:07 +0530

    deleted

--- Sklearn.py
+++
@@ -1,4 +0,0 @@
-#Model evaluation tools
-from sklearn.metrics import classification_report , accuracy_score , confusion_matrix
-from sklearn.metrics import accuracy_score,f1_score
-from sklearn.model_selection import cross_val_score
